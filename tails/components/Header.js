97f6a2e55653a204aa3badc2ead13022672a0439
ccarver80 <ckc_80@outlook.com>
2023-01-04 11:23:35 -0600

    header built

--- tails/components/Header.js
+++ tails/components/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Image from "next/image";
+import logo from "../styles/imgs/Logo.png";
 import styles from "../styles/Header.module.css";
+
 export const Header = () => {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <h1>Logo</h1>
+        <Image src={logo} className={styles.image} />
       </div>
     </div>
   );
