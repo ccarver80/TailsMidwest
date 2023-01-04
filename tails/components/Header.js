84e2081ed7164fa8c981bd6a913c59f395412a19
ccarver80 <ckc_80@outlook.com>
2023-01-04 10:40:15 -0600

    init next project, init tailwind, start header

---
+++ tails/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "../styles/Header.module.css";
+export const Header = () => {
+  return (
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <h1>Logo</h1>
+      </div>
+    </div>
+  );
+};
