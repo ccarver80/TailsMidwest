dfd7b3fc1759cd1d0316018e981a4f89212fded6
ccarver80 <ckc_80@outlook.com>
2023-01-04 20:00:13 -0600

    home page done

--- tails/components/Header.js
+++ tails/components/Header.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import logo from "../styles/imgs/Logo.png";
+import logo from "../public/imgs/Logo.png";
 import styles from "../styles/Header.module.css";
 
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>
-        <Image src={logo} className={styles.image} />
-      </div>
+      <Image src={logo} className={styles.header__image} />
     </div>
   );
 };
--- tails/pages/index.js
+++ tails/pages/index.js
@@ -1,7 +1,8 @@
-import Head from "next/head";
 import Image from "next/image";
-import { Header } from "../components/Header";
+import { Header, NavBar } from "../components";
 import { Inter } from "@next/font/google";
+import catDog from "../public/imgs/cat_dog.png";
+import logoText from "../public/imgs/logoText.png";
 import styles from "../styles/Home.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -10,7 +11,26 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <h1>Home</h1>
+      <NavBar />
+      <div className={styles.home}>
+        <div className={styles.home__header}>
+          <div>
+            <Image src={catDog} className={styles.home__catDogLogo} />
+            <Image src={logoText} className={styles.home__logo} />
+          </div>
+          <div className={styles.home__aboutUs}>
+            <h2>About Us</h2>
+            <p>
+              Founded in September of 2017, the purpose of Tails of the Midwest
+              Rescue is to take in unwanted, abandoned, abused, or stray pets
+              and attempt to find suitable forever-homes for them. This will
+              prevent possible neglect and/or euthanasia. This rescue is created
+              and run by local volunteers in the Bismarck-Mandan community. We
+              are a 501(c)3 non-profit organization, and fully foster-based.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
