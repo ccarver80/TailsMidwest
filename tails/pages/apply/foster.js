1fe75cbb528d67511a7e62a2044ed02dc6e62187
ccarver80 <ckc_80@outlook.com>
2023-01-14 09:32:14 -0600

    started foster form, built TextInput

--- tails/pages/apply/foster.js
+++ tails/pages/apply/foster.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Image from "next/image";
 import logo from "../../public/imgs/logoText.png";
 import { Header } from "../../components";
 import styles from "../../styles/Foster.module.css";
+import TextInput from "../../components/common/TextInput";
 
 export default function Foster() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   return (
     <div className={styles.foster}>
       <Header />
@@ -50,6 +57,122 @@ export default function Foster() {
           </li>
         </ul>
       </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.foster__form}>
+          <TextInput
+            lable="Applicants First Name:"
+            register={register}
+            name="first_name"
+            placeholder="Bob"
+          />
+
+          <TextInput
+            lable="Applicants Last Name:"
+            register={register}
+            name="last_name"
+            placeholder="Smith"
+          />
+          <div className="col-span-2 ">
+            <TextInput
+              lable="Email:"
+              register={register}
+              name="email"
+              type="email"
+              placeholder="bob_smith@email.com"
+            />
+          </div>
+
+          <TextInput
+            register={register}
+            lable="Co-Applicants First Name:"
+            name="co_app_first_name"
+            placeholder="Jane"
+          />
+
+          <TextInput
+            register={register}
+            lable="Co-Applicants Last Name:"
+            name="co_app_last_name"
+            placeholder="Smith"
+          />
+
+          <div className="col-span-2 ">
+            <TextInput
+              lable="Co-Applicant's Email:"
+              register={register}
+              name="co_email"
+              type="email"
+              placeholder="jane_smith@email.com"
+            />
+          </div>
+
+          <div className="col-span-2 ">
+            <TextInput
+              lable="Applicant's Address 1:"
+              register={register}
+              name="address_1"
+              placeholder="123 Apple St"
+            />
+          </div>
+
+          <div className="col-span-2 ">
+            <TextInput
+              lable="Applicant's Address 2:"
+              register={register}
+              name="address_2"
+              placeholder="Apt #123"
+            />
+          </div>
+
+          <TextInput
+            lable="City"
+            register={register}
+            name="city"
+            placeholder="Bismarck"
+          />
+          <TextInput
+            lable="State/Province"
+            register={register}
+            name="state"
+            placeholder="North Dakota"
+          />
+
+          <TextInput
+            lable="Zip Code"
+            register={register}
+            name="zip_code"
+            placeholder="58504"
+          />
+
+          <TextInput
+            lable="Country"
+            register={register}
+            name="country"
+            placeholder="United States"
+          />
+
+          <TextInput
+            lable="Applicants Phone:"
+            register={register}
+            name="phone"
+            placeholder="111-222-3456"
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+          />
+
+          <TextInput
+            lable="Co-Applicants Phone:"
+            register={register}
+            name="co_phone"
+            placeholder="111-222-3456"
+            type="tel"
+            pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
