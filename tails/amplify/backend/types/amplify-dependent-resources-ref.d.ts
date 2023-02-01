c59f61564fac09758d07fefccac2afbeadb8e16f
ccarver80 <ckc_80@outlook.com>
2023-02-01 17:02:29 -0600

    delete amplify

--- tails/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "tails": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
