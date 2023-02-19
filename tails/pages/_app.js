0c2c7196adc0c49aad8c8d34115bbef239bd547f
ccarver80 <ckc_80@outlook.com>
2023-02-19 15:13:57 -0600

    aws

--- tails/pages/_app.js
+++ tails/pages/_app.js
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
 import { Amplify } from "aws-amplify";
-import config from "../src/aws-exports";
 
-Amplify.configure({ ...config, ssr: true });
 export default function App({ Component, pageProps }) {
+  require("dotenv").config();
+  Amplify.configure({
+    aws_project_region: process.env.aws_project_region,
+    aws_appsync_graphqlEndpoint: process.env.aws_appsync_graphqlEndpoint,
+    aws_appsync_region: process.env.aws_appsync_region,
+    aws_appsync_authenticationType: process.env.aws_appsync_authenticationType,
+    aws_appsync_apiKey: process.env.aws_appsync_apiKey,
+    ssr: true,
+  });
+
   return <Component {...pageProps} />;
 }
