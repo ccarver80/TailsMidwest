9a416148e1372778feb14b99fe3646cedb1fcba9
ccarver80 <ckc_80@outlook.com>
2023-02-20 12:27:53 -0600

    config

--- tails/pages/_app.js
+++ tails/pages/_app.js
@@ -1,4 +1,7 @@
 import "../styles/globals.css";
+import { Amplify } from "aws-amplify";
+import awsmobile from "../src/aws-exports";
+Amplify.configure({ ...awsmobile, ssr: true });
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
