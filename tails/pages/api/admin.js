ab10e6ed116fef093ba406527529744d9b0edebc
ccarver80 <ckc_80@outlook.com>
2023-02-21 14:58:58 -0600

    comment out

--- tails/pages/api/admin.js
+++ tails/pages/api/admin.js
@@ -1,17 +1,17 @@
-import { DataStore } from "@aws-amplify/datastore";
-import { Post } from "../../src/models";
+// import { DataStore } from "@aws-amplify/datastore";
+// import { Post } from "../../src/models";
 
 export default async (req, res) => {
   if (req.method === "POST") {
-    await DataStore.save(
-      new Post({
-        title: req.body.Title,
-        description: req.body.Description,
-      })
-    );
+    // await DataStore.save(
+    //   new Post({
+    //     title: req.body.Title,
+    //     description: req.body.Description,
+    //   })
+    // );
     res.status(201).json({ message: "posted sucsesfully" });
   } else {
-    const data = await DataStore.query(Post);
+    // const data = await DataStore.query(Post);
     res.status(200).json(data);
   }
 };
