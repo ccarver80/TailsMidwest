4e91033934b5a304f274841625acbe70aacf6171
ccarver80 <ckc_80@outlook.com>
2023-02-21 14:51:16 -0600

    git push

--- tails/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "Tails2": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
