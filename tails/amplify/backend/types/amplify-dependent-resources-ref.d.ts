5cc67a3ee0a44d643c7eef954c92092e5778abff
ccarver80 <ckc_80@outlook.com>
2023-02-21 18:20:32 -0600

    amplify init

--- tails/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ tails/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "api": {
-    "TailsMidwesttails": {
+    "tails": {
       "GraphQLAPIEndpointOutput": "string",
       "GraphQLAPIIdOutput": "string",
       "GraphQLAPIKeyOutput": "string"
